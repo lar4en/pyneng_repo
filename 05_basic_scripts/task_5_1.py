8d219ce34db26c22d6a8013a5e12019ea2de31c4
lar4en <lar4en@gmail.com>
2018-05-11 13:39:15 +0700

    .

--- 05_basic_scripts/task_5_1.py
+++ 05_basic_scripts/task_5_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Задание 5.1
@@ -19,3 +20,38 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+import math
+
+input_string = input('\nEnter IP-network (<network>/<length>): ')
+
+network_inp, length_inp = input_string.split('/')
+
+octets_network = network_inp.split('.')
+for i in range( len(octets_network) ):
+    octets_network[i] = int(octets_network[i])
+
+print('\nNetwork:')
+print('{:<8d} {:<8d} {:<8d} {:<8d}'.format( octets_network[0], octets_network[1], octets_network[2], octets_network[3]) )
+print("{:08b} {:08b} {:08b} {:08b}".format( octets_network[0], octets_network[1], octets_network[2], octets_network[3]) )
+
+print('\nMask:')
+print("/{}".format(length_inp) )
+
+if int(length_inp):
+    mask = 0xFFFFFFFF >> (32 - int(length_inp) )
+else:
+    mask = 0
+mask = int("{:032b}".format(mask)[::-1], 2)
+
+mod         = [mask,0,0,0]
+mask_octets = [   0,0,0,0]
+for i in range( len( mask_octets ) ):
+    mask_octets[i] = int( math.floor( mod[i] / math.pow(256, 3-i) ) )
+    if i < 3:
+        mod[i+1]  = mod[i] % math.pow(256, 3-i )
+
+print('{:<8d} {:<8d} {:<8d} {:<8d}'.format(   mask_octets[0], mask_octets[1], mask_octets[2], mask_octets[3] ) )
+print("{:08b} {:08b} {:08b} {:08b}\n".format( mask_octets[0], mask_octets[1], mask_octets[2], mask_octets[3] ) )
+
+
--- 05_basic_scripts/task_5_1b.py
+++ 05_basic_scripts/task_5_1b.py
@@ -1,11 +1,62 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-Задание 5.1b
+"""
+Задание 4.1b
 
-Преобразовать скрипт из задания 5.1a таким образом,
-чтобы сеть/маска не запрашивались у пользователя,
+Преобразовать скрипт из задания 4.1a таким образом, чтобы сеть/маска не запрашивались у пользователя,
 а передавались как аргумент скрипту.
+"""
 
-Ограничение: Все задания надо выполнять используя только пройденные темы.
+import math
+from sys import argv
 
-'''
+# input_string = raw_input('\nEnter IP-network (<network>/<length>): ')
+input_string = argv[1]
+
+network_inp, length_inp = input_string.split('/')
+length_inp = int(length_inp)
+
+# Mask Manipulation
+if length_inp:
+    mask = 0xFFFFFFFF >> (32 - length_inp )
+    mask = mask << (32 - length_inp )
+else:
+    mask = 0
+
+mod         = [mask,0,0,0]
+mask_octets = [   0,0,0,0]
+for i in range( len( mask_octets ) ):
+    mask_octets[i] = int( math.floor( mod[i] / math.pow(256, 3-i) ) )
+    if i < 3:
+        mod[i+1]  = mod[i] % math.pow(256, 3-i )
+
+# Network Manipulation
+octets_network = network_inp.split('.')
+for i in range( len(octets_network) ):
+    octets_network[i] = int(octets_network[i])
+
+print( '\nHost:' )
+print( '{:<8d} {:<8d} {:<8d} {:<8d}'.format( octets_network[0], octets_network[1], octets_network[2], octets_network[3]) )
+print( "{:08b} {:08b} {:08b} {:08b}".format( octets_network[0], octets_network[1], octets_network[2], octets_network[3]) )
+
+network = "{:08b}{:08b}{:08b}{:08b}".format( octets_network[0], octets_network[1], octets_network[2], octets_network[3])
+network = int(network, 2)
+network = network >> (32 - length_inp)
+network = network << (32 - length_inp)
+
+mod[0] = network
+for i in range( len(octets_network) ):
+    octets_network[i] = int( math.floor( mod[i] / math.pow(256, 3-i) ) )
+    if i < 3:
+        mod[i+1] = mod[i] % math.pow(256, 3-i)
+
+# OUTPUT
+print( '\nNetwork:')
+print( '{:<8d} {:<8d} {:<8d} {:<8d}'.format( octets_network[0], octets_network[1], octets_network[2], octets_network[3]) )
+print( "{:08b} {:08b} {:08b} {:08b}".format( octets_network[0], octets_network[1], octets_network[2], octets_network[3]) )
+
+print( '\nMask:' )
+print( "/{}".format(length_inp) )
+print( '{:<8d} {:<8d} {:<8d} {:<8d}'.format(   mask_octets[0], mask_octets[1], mask_octets[2], mask_octets[3] ) )
+print( "{:08b} {:08b} {:08b} {:08b}\n".format( mask_octets[0], mask_octets[1], mask_octets[2], mask_octets[3] ) )
+
